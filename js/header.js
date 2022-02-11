c8232f7146f5dfa054553c89f9163e3d7ef0bbbe
AtulPant2704 <atulpant1999@gmail.com>
2022-02-11 14:49:36 +0530

    feat: add opacity on menu-bar active

--- js/header.js
+++ js/header.js
@@ -1,10 +1,12 @@
 const menuOpenAndClose = document.querySelector(".fa-bars");
 const menuItems = document.querySelector("aside");
-const mainSection = document.querySelector("main")
+const fullBackground = document.querySelector(".background-container");
 
 menuOpenAndClose.addEventListener("click", openMenuItems);
+fullBackground.addEventListener("click",openMenuItems);
 
 function openMenuItems(event) {
     menuItems.classList.toggle("nav-open");
     menuOpenAndClose.classList.toggle("fa-times");
+    fullBackground.classList.toggle("open");
 }
