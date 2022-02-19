1e2c1c6b0e900963913413b4d666f61f93d4aaaf
AtulPant2704 <atulpant1999@gmail.com>
2022-02-19 13:30:29 +0530

    fix: open menu bar on screen click

--- js/header.js
+++ js/header.js
@@ -3,10 +3,16 @@ const menuItems = document.querySelector("aside");
 const fullBackground = document.querySelector(".background-container");
 
 menuOpenAndClose.addEventListener("click", openMenuItems);
-fullBackground.addEventListener("click",openMenuItems);
+fullBackground.addEventListener("click",closeMenuItems);
 
-function openMenuItems(event) {
+function openMenuItems() {
     menuItems.classList.toggle("nav-open");
     menuOpenAndClose.classList.toggle("fa-times");
     fullBackground.classList.toggle("open");
+}
+
+function closeMenuItems(){
+    menuItems.classList.remove("nav-open");
+    menuOpenAndClose.classList.remove("fa-times");
+    fullBackground.classList.remove("open");
 }
