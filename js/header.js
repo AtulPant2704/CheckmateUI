8493e20904e15e391bdd9d98e062bf115184057d
AtulPant2704 <atulpant1999@gmail.com>
2022-02-19 17:52:42 +0530

    fix: menu items close on click

--- js/header.js
+++ js/header.js
@@ -1,9 +1,11 @@
 const menuOpenAndClose = document.querySelector(".fa-bars");
 const menuItems = document.querySelector("aside");
 const fullBackground = document.querySelector(".background-container");
+const fullView = document.querySelector("body");
 
 menuOpenAndClose.addEventListener("click", openMenuItems);
 fullBackground.addEventListener("click",closeMenuItems);
+fullView.addEventListener("click", closeMenuItems);
 
 function openMenuItems() {
     menuItems.classList.toggle("nav-open");
@@ -11,8 +13,10 @@ function openMenuItems() {
     fullBackground.classList.toggle("open");
 }
 
-function closeMenuItems(){
-    menuItems.classList.remove("nav-open");
-    menuOpenAndClose.classList.remove("fa-times");
-    fullBackground.classList.remove("open");
+function closeMenuItems(event){
+    if(event.target != menuItems && event.target != menuOpenAndClose){
+        menuItems.classList.remove("nav-open");
+        menuOpenAndClose.classList.remove("fa-times");
+        fullBackground.classList.remove("open");
+    }
 }
