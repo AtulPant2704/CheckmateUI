1b901ad1f6db76d958a0a01d0e6fc628f63d1162
AtulPant2704 <atulpant1999@gmail.com>
2022-02-20 17:23:07 +0530

    feat: add hamburger

---
+++ components/Hamburger/hamburger.js
@@ -0,0 +1,15 @@
+const menuBarBtn = document.querySelector(".menu-bar");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const menuBarClose = document.querySelector(".menu-bar-close");
+
+menuBarBtn.addEventListener("click",openMenuBar);
+menuBarClose.addEventListener("click",closeMenuBar);
+hamburgerBackground.addEventListener("click",closeMenuBar);
+
+function openMenuBar(){
+   hamburgerMenu.classList.add("hamburger-menu-open");
+}
+
+function closeMenuBar(){
+   hamburgerMenu.classList.remove("hamburger-menu-open");
+}
